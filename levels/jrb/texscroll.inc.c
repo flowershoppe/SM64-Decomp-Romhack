c704c43180e7b1bb5f6a6a80eff1db5fcc32aecc
flowershoppe <163602137+flowershoppe@users.noreply.github.com>
2026-08-04 19:26:28 -0700

    checkpoint fix

--- levels/jrb/texscroll.inc.c
+++ levels/jrb/texscroll.inc.c
@@ -549,6 +549,35 @@ void scroll_jrb_dl_Cube_059_mesh_layer_1_vtx_1() {
 	currentX += deltaX;	currentY += deltaY;
 }
 
+void scroll_jrb_dl_Cube_072_mesh_layer_1_vtx_1() {
+	int i = 0;
+	int count = 7;
+	int width = 32 * 0x20;
+	int height = 32 * 0x20;
+
+	static int currentX = 0;
+	int deltaX;
+	static int currentY = 0;
+	int deltaY;
+	Vtx *vertices = segmented_to_virtual(jrb_dl_Cube_072_mesh_layer_1_vtx_1);
+
+	deltaX = (int)(0.20000000298023224 * 0x20) % width;
+	deltaY = (int)(0.20000000298023224 * 0x20) % height;
+
+	if (absi(currentX) > width) {
+		deltaX -= (int)(absi(currentX) / width) * width * signum_positive(deltaX);
+	}
+	if (absi(currentY) > height) {
+		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[0] += deltaX;
+		vertices[i].n.tc[1] += deltaY;
+	}
+	currentX += deltaX;	currentY += deltaY;
+}
+
 void scroll_jrb() {
 	scroll_jrb_dl_render_027_mesh_layer_1_vtx_1();
 	scroll_jrb_dl_Cube_020_mesh_layer_1_vtx_1();
@@ -569,4 +598,5 @@ void scroll_jrb() {
 	scroll_jrb_dl_Cube_057_mesh_layer_1_vtx_1();
 	scroll_jrb_dl_Cube_058_mesh_layer_1_vtx_1();
 	scroll_jrb_dl_Cube_059_mesh_layer_1_vtx_1();
+	scroll_jrb_dl_Cube_072_mesh_layer_1_vtx_1();
 };
